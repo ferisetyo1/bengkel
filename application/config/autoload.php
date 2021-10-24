f53bee84384bd7f5c03c4681be1f1076696005bc
ferisetyo1 <ferisetyo1@gmail.com>
2021-10-25 06:06:33 +0700

    a

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Dbhelper','datatable');
+$autoload['model'] = array('dbhelper','datatable');
